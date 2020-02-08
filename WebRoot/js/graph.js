de3acce6b08a7201e16607ca14665fe15a56418f
Yi Zhao <yzhao156@uottawa.ca>
2020-02-08 11:29:16 -0500

    Changed colors for different graph, added death&curied graph

--- WebRoot/js/graph.js
+++ WebRoot/js/graph.js
@@ -1,3 +1,4 @@
+var colors = ['#5793f3', '#d14a61','#86ceff','#ff686d', '#6e2b28','#022463'];
 /*
  * Drawing Increment Confirmed and Suspected
  */
@@ -15,12 +16,13 @@ function drawIncConfSusp(history) {
     }
     console.log(suspectedIncr);
     console.log(confirmedIncr);
-    drawIncConfSuspGraph(suspectedIncr, confirmedIncr, "icsContainer");
+    drawCurve(suspectedIncr, confirmedIncr, "icsContainer",'Incresed Suspected Patient', 'Increased Confirmed Patient',0,1);
 }
 
 function drawConfSusp(history) {
     // console.log("Drawing Increment Confirmed and Suspected");
-    // console.log(history);
+    console.log(history);
+    console.log(history);
     //get data into map
     var suspected = {}; //year:suspected
     var confirmed = {}; //year:confirmed
@@ -32,19 +34,32 @@ function drawConfSusp(history) {
     }
     console.log(suspected);
     console.log(confirmed);
-    drawIncConfSuspGraph(suspected, confirmed, "csContainer");
+    drawCurve(suspected, confirmed, "csContainer",'Total Suspected Patient', 'Total Confirmed Patient',2,3);
 }
 
-function drawIncConfSuspGraph(suspectedIncr, confirmedIncr, container) {
+function drawCuresDeath(history){
+    //get data into map
+    var curesNum = {}; //year:suspected
+    var deathsNum = {}; //year:confirmed
+
+    for (var i = history.length - 1; i >= 0; i--) {
+        // console.log(i);
+        curesNum[history[i].date] = history[i].curesNum;
+        deathsNum[history[i].date] = history[i].deathsNum;
+    }
+    console.log(curesNum);
+    console.log(deathsNum);
+    drawCurve(curesNum, deathsNum, "cdContainer",'Total Cured Patient', 'Total Death Patient',4,5);
+}
+
+function drawCurve(mapping1, mapping2, container,legendName1,legendName2,color1,color2) {
     var dom = document.getElementById(container);
     var myChart = echarts.init(dom);
     var app = {};
     option = null;
-    var colors = ['#5793f3', '#d14a61', '#675bba'];
-
 
     option = {
-        color: colors,
+        color: [colors[color1],colors[color2]],
 
         tooltip: {
             trigger: 'none',
@@ -53,7 +68,7 @@ function drawIncConfSuspGraph(suspectedIncr, confirmedIncr, container) {
             }
         },
         legend: {
-            data: ['suspected patient', 'confirmed patient']
+            data: [legendName1, legendName2]
         },
         grid: {
             top: 70,
@@ -78,8 +93,7 @@ function drawIncConfSuspGraph(suspectedIncr, confirmedIncr, container) {
                         }
                     }
                 },
-                data: Object.keys(suspectedIncr)
-                    // data: ['2016-1', '2016-2', '2016-3', '2016-4', '2016-5', '2016-6', '2016-7', '2016-8', '2016-9', '2016-10', '2016-11', '2016-12']
+                data: Object.keys(mapping1)
             },
             {
                 type: 'category',
@@ -101,24 +115,23 @@ function drawIncConfSuspGraph(suspectedIncr, confirmedIncr, container) {
                     }
                 },
                 data: []
-                    // data: ['2015-1', '2015-2', '2015-3', '2015-4', '2015-5', '2015-6', '2015-7', '2015-8', '2015-9', '2015-10', '2015-11', '2015-12']
             }
         ],
         yAxis: [{
             type: 'value'
         }],
         series: [{
-                name: 'suspected patient',
+                name: legendName1,
                 type: 'line',
                 xAxisIndex: 1,
                 smooth: true,
-                data: Object.values(suspectedIncr)
+                data: Object.values(mapping1)
             },
             {
-                name: 'confirmed patient',
+                name: legendName2,
                 type: 'line',
                 smooth: true,
-                data: Object.values(confirmedIncr)
+                data: Object.values(mapping2)
             }
         ]
     };;
--- WebRoot/js/request.js
+++ WebRoot/js/request.js
@@ -21,5 +21,6 @@ $(document).ready(function() {
             }
             drawIncConfSusp(history);
             drawConfSusp(history);
+            drawCuresDeath(history);
         });
 });
